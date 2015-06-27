0664120fda804072366f6cec7ef24782d06f6cca
Kevin C. Krinke <kevin@krinke.ca>
2015-06-26 21:05:41 -0400

    added some helpful constants, implemented otrl user state static data bits.

--- plugins/otrm/otrm.c
+++ plugins/otrm/otrm.c
@@ -2,6 +2,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <libotr/proto.h>
+#include <libotr/message.h>
+#include <libotr/userstate.h>
+
+#define True 1
+#define False 0
+#define RV_SUCCESS 1
+#define RV_ERROR 0
 
 /** @file plugins/otrm/otrm.c OTR implementation for hexchat
  *
@@ -13,18 +20,18 @@
 #define PDESC "Off The Record Messaging"
 #define PVERSION "1.0"
 
-static hexchat_plugin *ph;      /* plugin handle */
+static hexchat_plugin *g_plugin; ///< plugin handle
+static OtrlUserState *g_userstate;  ///< OTR User State
 
 static int
 otrm_cb (char *word[], char *word_eol[], void *userdata)
 {
-  if (strncmp(word[2],"help",4)==0) {
-    hexchat_print (ph, "usage: /OTRM {help|version}\n");
-  }
-  else if (strncmp(word[2],"version",7)==0) {
-    hexchat_print (ph, "OTRM v"PVERSION"\n");
+  if (strncmp(word[2],"version",7)==0) {
+    hexchat_print (g_plugin, "OTRM Plugin v"PVERSION", libotr v"OTRL_VERSION"\n");
+    return HEXCHAT_EAT_ALL; /* eat this command so HexChat and other plugins can't process it */
   }
 
+  hexchat_print (g_plugin, "usage: /otrm {help|version}\n");
   return HEXCHAT_EAT_ALL; /* eat this command so HexChat and other plugins can't process it */
 }
 
@@ -41,17 +48,27 @@ hexchat_plugin_init (hexchat_plugin *plugin_handle, char **plugin_name, char **p
 {
   OTRL_INIT;
 
+  g_userstate = otrl_userstate_create();
+
   /* we need to save this for use with any hexchat_* functions */
-  ph = plugin_handle;
+  g_plugin = plugin_handle;
 
   /* tell HexChat our info */
   *plugin_name = PNAME;
   *plugin_desc = PDESC;
   *plugin_version = PVERSION;
 
-  hexchat_hook_command (ph, "otrm", HEXCHAT_PRI_NORM, otrm_cb, "Usage: /OTRM {help|version}", 0);
+  hexchat_hook_command (g_plugin, "otrm", HEXCHAT_PRI_NORM, otrm_cb, "Usage: /OTRM {help|version}", 0);
 
-  hexchat_print (ph, PNAME" loaded successfully!\n");
+  hexchat_print (g_plugin, PNAME" plugin loaded.\n");
 
+  return 1;       /* return 1 for success */
+}
+
+int
+hexchat_plugin_deinit (void)
+{
+  otrl_userstate_free(*g_userstate);
+  hexchat_print (g_plugin, PNAME" plugin unloaded.\n");
   return 1;       /* return 1 for success */
 }
