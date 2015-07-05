8a8ee4a29a7772ecc53b26c20274002cd5097ade
Kevin C. Krinke <kevin@krinke.ca>
2015-07-05 01:15:23 -0400

    new constants header

---
+++ plugins/otrm/constants.h
@@ -0,0 +1,23 @@
+/** @file otrm/constants.h  Global defines and macros
+ */
+
+#ifndef _OTRM_CONSTANTS_H_
+#define _OTRM_CONSTANTS_H_
+
+#include "hexchat-plugin.h"
+#include <stdio.h>
+#include <string.h>
+#include <libotr/proto.h>
+#include <libotr/message.h>
+#include <libotr/userstate.h>
+
+#define True 1
+#define False 0
+#define RV_SUCCESS 1
+#define RV_ERROR 0
+
+#define PLUGIN_NAME "OTRM"
+#define PLUGIN_DESC "Off The Record Messaging"
+#define PLUGIN_VERSION "0.2"
+
+#endif //_OTRM_CONSTANTS_H_
